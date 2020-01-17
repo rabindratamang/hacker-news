f91d791529a45bc1f4ffe418c876741163cf3e5f
Rabindra Tamang <rabindratamang147@gmail.com>
2020-01-17 22:46:20 +0545

    Add search input and handler

--- src/App.js
+++ src/App.js
@@ -20,16 +20,25 @@ const list = [
   }
 ];
 
+//higher order function
+function isSearched(searchTerm) {
+  return function(item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  };
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     //binding list to App class local state
     this.state = {
-      list //equivalent of list: list because they share same name
+      list, //equivalent of list: list because they share same name (es6)
+      searchTerm: ""
     };
 
     this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onDismiss(id) {
@@ -40,29 +49,44 @@ class App extends Component {
     }
   }
 
-  //map  creates new array objectwith value from callback
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  //map  creates new array object with value from callback
   render() {
-    let { list } = this.state;
+    let { list, searchTerm } = this.state;
     return (
       <div className="App">
-        {list.map(item => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type="button"
-              >
-                X
-              </button>
-            </span>
-          </div>
-        ))}
+        <form>
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={this.onSearchChange}
+            value={searchTerm}
+          />
+        </form>
+
+        {list.filter(isSearched(searchTerm)).map(item => {
+          //as function with parenthesis invokes immediately so a wrapper is added
+          const onHandleDismiss = () => this.onDismiss(item.objectID);
+
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+              <span>
+                <button onClick={onHandleDismiss} type="button">
+                  X
+                </button>
+              </span>
+            </div>
+          );
+        })}
       </div>
     );
   }
