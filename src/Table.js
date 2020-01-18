5f716f155cd18b7aac477ba29f3074e50ceb84b3
Rabindra Tamang <rabindratamang147@gmail.com>
2020-01-18 22:37:03 +0545

    Changed components to es6 functional stateless components

--- src/Table.js
+++ src/Table.js
@@ -1,39 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import Button from "./Button";
 
-//higher order function
-function isSearched(searchTerm) {
-  return function(item) {
-    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-  };
-}
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-class Table extends Component {
-  render() {
-    const { list, pattern, onDismiss } = this.props;
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item => (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-            <span>
-              <Button
-                onClick={() => onDismiss(item.objectID)}
-                className="btn btn-sucess"
-              >
-                x
-              </Button>
-            </span>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+const Table = ({ list, pattern, onDismiss }) => {
+  return (
+    <div>
+      {list.filter(isSearched(pattern)).map(item => (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+            <Button onClick={() => onDismiss(item.objectID)}>x</Button>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Table;
