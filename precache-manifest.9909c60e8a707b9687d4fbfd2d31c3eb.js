728a573ef6affb9fa444f4cb17f092fcc547a370
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-27 06:29:22 +0000

    Deploy rabindratamang/hacker-news to github.com/rabindratamang/hacker-news.git:gh-pages

---
+++ precache-manifest.9909c60e8a707b9687d4fbfd2d31c3eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502796dfe88be217a7f0eecc65cd8ebd",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "e16d399b5c9f2552f76b",
+    "url": "/hacker-news/static/css/main.044dc2c6.chunk.css"
+  },
+  {
+    "revision": "7279402b7b76f10c2dea",
+    "url": "/hacker-news/static/js/2.38894828.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hacker-news/static/js/2.38894828.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e16d399b5c9f2552f76b",
+    "url": "/hacker-news/static/js/main.1cac1317.chunk.js"
+  },
+  {
+    "revision": "4ba2fd5c45dab88f1e78",
+    "url": "/hacker-news/static/js/runtime-main.e7985a32.js"
+  }
+]);
